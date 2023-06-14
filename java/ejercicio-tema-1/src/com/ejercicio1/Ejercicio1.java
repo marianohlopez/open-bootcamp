d3c1694fc1799b2974c3c628820b9747d348fcb1
mlopez <ml.3012@gmail.com>
2023-06-14 08:43:41 -0300

    java-1

---
+++ java/ejercicio-tema-1/src/com/ejercicio1/Ejercicio1.java
@@ -0,0 +1,18 @@
+package com.ejercicio1;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        int n = 5;
+        long n2 = 5000;
+        boolean b = true;
+        String s = "hola mundo";
+        char a = 'a';
+        double d = 10.5;
+        System.out.println(n);
+        System.out.println(n2);
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(a);
+        System.out.println(d);
+    }
+}
