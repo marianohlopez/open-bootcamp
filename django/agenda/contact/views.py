8edee593f163b328d57ca6d2c134552d75969910
mlopez <ml.3012@gmail.com>
2023-08-11 13:26:06 -0300

    delete-todo

--- django/agenda/contact/views.py
+++ django/agenda/contact/views.py
@@ -57,3 +57,9 @@ def create(request):
         if form.is_valid():
             form.save()
         return redirect("contact")
+
+
+def delete(request, id):
+    contact = Contact.objects.get(id=id)
+    contact.delete()
+    return redirect("contact")
--- django/agenda/todo/views.py
+++ django/agenda/todo/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from .models import Todo
 
 
 def index(request):
+    todos = Todo.objects.filter(title__contains=request.GET.get("search", ""))
+    context = {
+        "todos": todos
+    }
+    return render(request, "todo/index.html", context)
+
+
+def view(request, id):
+    return render(request, "todo/index.html", {})
+
+
+def edit(request, id):
+    return render(request, "todo/index.html", {})
+
+
+def delete(request, id):
+    return render(request, "todo/index.html", {})
+
+
+def create(request, id):
     return render(request, "todo/index.html", {})
